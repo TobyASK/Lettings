f5b847cb55b72430b39b606e8eb9c4bae5b599c6
TobyASK <bekkouche.anis06@gmail.com>
2026-08-05 00:10:44 +0200

    Force l'event Sentry sur la route debug

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -2,6 +2,7 @@
 
 import logging
 
+import sentry_sdk
 from django.shortcuts import render
 
 logger = logging.getLogger(__name__)
@@ -28,4 +29,5 @@ def custom_500(request):
 def sentry_debug(request):
     """Raise an exception to test Sentry integration."""
     logger.error('Sentry debug route called')
+    sentry_sdk.capture_message('Sentry debug route called')
     raise RuntimeError('Sentry test exception from /sentry-debug/')
